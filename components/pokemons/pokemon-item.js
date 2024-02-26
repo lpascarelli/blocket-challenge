be056c94e52dcb89390b6b80f232d906fc32bfde
lpascarelli <luca.pascarelli@brocc.se>
2024-02-26 16:56:57 +0100

    Fix some css styling and onClick event

--- components/pokemons/pokemon-item.js
+++ components/pokemons/pokemon-item.js
@@ -1,23 +1,27 @@
 import Image from 'next/image';
-import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 import Card from '../ui/card';
 import { POKEMON_IMAGE_URL } from '../../constants';
 import classes from './pokemon-item.module.css';
 
 function PokemonItem(props) {
+  const router = useRouter();
+
+  const goToPokemonPage = () => {
+    router.push(`/pokemon/${props.id}`);
+  }
+
   return (
-    <Card>
-      <Link href={`/pokemon/${props.id}`}>
-        <div className={classes['pokemon-image']}>
-          <Image
-            src={`${POKEMON_IMAGE_URL}/${props.id}.png`}
-            alt={props.name}
-            width={150}
-            height={150}
-          />
-        </div>
-      </Link>
+    <Card onClick={goToPokemonPage}>
+      <div className={classes['pokemon-image']}>
+        <Image
+          src={`${POKEMON_IMAGE_URL}/${props.id}.png`}
+          alt={props.name}
+          width={150}
+          height={150}
+        />
+      </div>
       <span className={classes['pokemon-number']}>N° {props.id}</span>
       <h3 className={classes['pokemon-name']}>{props.name}</h3>
     </Card>
--- components/ui/card.js
+++ components/ui/card.js
@@ -4,6 +4,7 @@ function Card(props) {
   return (
     <div
       className={`${classes['card-container']} ${classes.card} ${classes.flexed}`}
+      onClick={props.onClick}
     >
       {props.children}
     </div>
